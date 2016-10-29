602799474a7ed2cd292ec1eb9f3ffc3686591538
Frank A. Krueger <fak@praeclarum.org>
2016-10-29 16:50:01 -0700

    Add Amazon builtins

---
+++ AmazonIntents.cs
@@ -0,0 +1,18 @@
+namespace Amazon
+{
+    public class CancelIntent {}
+    public class HelpIntent {}
+    // public class LoopOffIntent {}
+    // public class LoopOnIntent {}
+    // public class NextIntent {}
+    public class NoIntent {}
+    // public class PauseIntent {}
+    // public class PreviousIntent {}
+    // public class RepeatIntent {}
+    // public class ResumeIntent {}
+    // public class ShuffleOffIntent {}
+    // public class ShuffleOnIntent {}
+    // public class StartOverIntent {}
+    // public class StopIntent {}
+    public class YesIntent {}
+}
---
+++ AmazonSlots.cs
@@ -0,0 +1,20 @@
+namespace Amazon
+{
+    public class AtCitySlot {}
+    public class AtRegionSlot {}
+    public class DateSlot {}
+    public class DeCitySlot {}
+    public class DeFirstNameSlot {}
+    public class DeRegionSlot {}
+    public class DurationSlot {}
+    public class EuropeCitySlot {}
+    public class FourDigitNumberSlot {}
+    public class GbCitySlot {}
+    public class GbFirstNameSlot {}
+    public class GbRegionSlot {}
+    public class NumberSlot {}
+    public class TimeSlot {}
+    public class UsCitySlot {}
+    public class UsFirstNameSlot {}
+    public class UsStateSlot {}
+}
