856eba6943d63592d5ea3b9e0a1652d123f3f5ac
Frank A. Krueger <fak@praeclarum.org>
2016-10-29 16:34:45 -0700

    Model EchoService

--- EchoService.cs
+++ EchoService.cs
@@ -37,4 +37,34 @@ namespace EchoService
         public string Type = "PlainText";
         public string Text = "";
     }
+    public class EchoIntentInfo
+    {
+        public string Intent = "";
+        public EchoIntentSlot[] Slots = new EchoIntentSlot[0];
+    }
+    public class EchoIntentSlot
+    {
+        public string Name = "";
+        public string Type = "";
+    }
+    public class EchoIntentSchema
+    {
+        public EchoIntent[] Intents = new EchoIntent[0];
+    }
+    public class EchoSkill
+    {
+        public EchoIntentSchema IntentSchema;
+        public EchoCustomSlotType CustomSlotTypes;
+        public EchoSampleUtterance[] SampleUtterances = new EchoSampleUtterance[0];
+    }
+    public class EchoSampleUtterance
+    {
+        public string Intent = "";
+        public string Utterance = "";
+    }
+    public class EchoCustomSlotType
+    {
+        public string Name = "";
+        public string[] Values = new string[0];
+    }
 }
