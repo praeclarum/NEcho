6b2132042e60a1989532f8f64936419da0fc5695
Frank A. Krueger <fak@praeclarum.org>
2016-10-29 15:04:34 -0700

    Add Echo response

--- HomeController.cs
+++ HomeController.cs
@@ -25,8 +25,15 @@ namespace WebApplication.Controllers
  
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public EchoServiceResponse Post([FromBody]EchoServiceRequest request)
         {
+            Console.WriteLine ("GOT REQUEST " + request.Request.Intent.Name);
+            return new EchoServiceResponse {
+Version="1.0",
+Response=new EchoResponse {
+OutputSpeech = new EchoSpeech { Type = "PlainText", Text = "Hello World" }
+}
+};
         }
  
         // PUT api/values/5
@@ -41,5 +48,40 @@ namespace WebApplication.Controllers
         {
         }
     }
+
+    public class EchoServiceRequest
+{
+public string Version;
+public EchoSession Session;
+public EchoRequest Request;
+}
+public class EchoSession
+{
+public string SessionId;
+}
+public class EchoRequest
+{
+public EchoIntent Intent;
+}
+public class EchoIntent
+{
+public string Name;
+}
+public class EchoServiceResponse
+{
+public string Version = "1.0";
+public Dictionary<string, dynamic> SessionAttributes = new Dictionary<string, dynamic>();
+public EchoResponse Response;
+}
+public class EchoResponse
+{
+public EchoSpeech OutputSpeech;
+public bool ShouldEndSession = true;
+}
+public class EchoSpeech
+{
+public string Type = "PlainText";
+public string Text = "";
+}
 }
 
