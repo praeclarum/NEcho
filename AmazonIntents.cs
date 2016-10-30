ee4a522e0859f1ea0329ccafe838d6de46bcc75d
Frank A. Krueger <fak@praeclarum.org>
2016-10-29 20:57:01 -0700

    Make Amazon Intents Intents

--- AmazonIntents.cs
+++ AmazonIntents.cs
@@ -1,18 +1,9 @@
+using EchoService;
+
 namespace Amazon
 {
-    public class CancelIntent {}
-    public class HelpIntent {}
-    // public class LoopOffIntent {}
-    // public class LoopOnIntent {}
-    // public class NextIntent {}
-    public class NoIntent {}
-    // public class PauseIntent {}
-    // public class PreviousIntent {}
-    // public class RepeatIntent {}
-    // public class ResumeIntent {}
-    // public class ShuffleOffIntent {}
-    // public class ShuffleOnIntent {}
-    // public class StartOverIntent {}
-    // public class StopIntent {}
-    public class YesIntent {}
+    public class CancelIntent : Intent {}
+    public class HelpIntent : Intent {}
+    public class NoIntent : Intent {}
+    public class YesIntent : Intent {}
 }
